4a19ce438c8052a0c1089c86c22ed7e77a6b0da3
mrahman <mrahmanjewel@gmail.com>
2018-01-14 13:46:41 +0900

    Fix order

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -24,11 +24,6 @@ then
   ln -s /vagrant/html /var/www
 fi
 
-
-docker exec -it webapp bash -c 'cd /var/www/site/smartschool ; php artisan migrate:refresh --seed'
-docker exec -it database mysql -e "GRANT ALL PRIVILEGES ON smartschool.* TO smartschool@'%' IDENTIFIED BY 'sm@rtsch00l' WITH GRANT OPTION"
-
-
 if [ ! "$(docker network ls | grep smartschool_net)" ]
 then
   docker network create smartschool_net
@@ -43,3 +38,6 @@ if [ ! "$(docker network inspect smartschool_net | grep database)" ]
 then
   docker network connect smartschool_net database
 fi
+
+docker exec -it webapp bash -c 'cd /var/www/site/smartschool ; php artisan migrate:refresh --seed'
+docker exec -it database mysql -e "GRANT ALL PRIVILEGES ON smartschool.* TO smartschool@'%' IDENTIFIED BY 'sm@rtsch00l' WITH GRANT OPTION"
